cbe6e0f3e736b24991d90e047f58af0128fe5ab4
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-05 17:44:37 +0530

    Update ATM_check.java

--- ATM_check.java
+++ ATM_check.java
@@ -1,13 +1,44 @@
-/*
-ATM Multi-Level Exception System
+public class ATMException {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
 
-Create a program that simulates ATM withdrawal.
+	        try {
+	            System.out.print("Enter Account Balance: ");
+	            double balance = sc.nextDouble();
 
-Requirements:
-Take balance and withdrawal amount
-If withdrawal > balance → handle exception
-If withdrawal amount is negative → handle exception
-If input is invalid (text instead of number) → handle exception
-Expected Output:
-Transaction Successful / Insufficient Balance / Invalid Input
-*/
+	            System.out.print("Enter Withdrawal Amount: ");
+	            double withdraw = sc.nextDouble();
+
+	            if (withdraw < 0) {
+	                throw new NegativeAmountException("Withdrawal amount cannot be negative.");
+	            }
+
+	            if (withdraw > balance) {
+	                throw new InsufficientBalanceException("Insufficient Balance.");
+	            }
+
+	            balance = balance - withdraw;
+
+	            System.out.println("Transaction Successful");
+	            System.out.println("Remaining Balance: " + balance);
+	        }
+
+	        catch (InputMismatchException e) {
+	            System.out.println("Invalid Input");
+	        }
+
+	        catch (NegativeAmountException e) {
+	            System.out.println(e.getMessage());
+	        }
+
+	        catch (InsufficientBalanceException e) {
+	            System.out.println(e.getMessage());
+	        }
+
+	        finally {
+	            sc.close();
+	        }
+	    }
+	}
+
+
